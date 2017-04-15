b3f3a8a9a429471a77c030b2d124eedf8cb483d9
brand <brand@DESKTOP-4RGNN06>
2017-04-15 15:12:17 -0500

    Add project files.

---
+++ GMS_FileDrop/GMS_Win32Ext.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef _GMS_Win32Ext_HPP
+#define _GMS_Win32Ext_HPP
+#define GMS_DLL extern "C" __declspec (dllexport)
+#include <Windows.h>
+#include <iostream>
+//DsMap creation to pass into GMS
+const int EVENT_OTHER_SOCIAL = 70;
+void(*CreateAsynEventWithDSMap)(int, int) = NULL;
+int(*CreateDsMap)(int _num, ...) = NULL;
+bool(*DsMapAddDouble)(int _index, char *_pKey, double value) = NULL;
+bool(*DsMapAddString)(int _index, char *_pKey, char *pVal) = NULL;
+
+GMS_DLL void RegisterCallbacks(char* arg1, char* arg2, char* arg3, char* arg4) {
+	void(*CreateAsynEventWithDSMapPtr)(int, int) = (void(*)(int, int))(arg1);
+	int(*CreateDsMapPtr)(int _num, ...) = (int(*)(int _num, ...))(arg2);
+	bool(*DsMapAddDoublePtr)(int _index, char* _pKey, double value) = (bool(*)(int, char*, double))(arg3);
+	bool(*DsMapAddStringPtr)(int _index, char* _pKey, char* value) = (bool(*)(int, char*, char*))(arg4);
+	CreateAsynEventWithDSMap = CreateAsynEventWithDSMapPtr;
+	CreateDsMap = CreateDsMapPtr;
+	DsMapAddDouble = DsMapAddDoublePtr;
+	DsMapAddString = DsMapAddStringPtr;
+}
+
+void ErrorCheck() {
+	std::cout << "Error: " << GetLastError() << std::endl;
+}
+
+// Convert a wide Unicode string to an UTF8 string
+std::string utf8_encode(const std::wstring &wstr)
+{
+	if (wstr.empty()) return std::string();
+	int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+	std::string strTo(size_needed, 0);
+	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+	return strTo;
+}
+// Convert an UTF8 string to a wide Unicode String
+std::wstring utf8_decode(const std::string &str)
+{
+	if (str.empty()) return std::wstring();
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+	std::wstring wstrTo(size_needed, 0);
+	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+	return wstrTo;
+}
+#endif
---
+++ GMS_FileDrop/main.cpp
@@ -0,0 +1,94 @@
+#include "GMS_Win32Ext.hpp"
+#include <vector>
+#include <string>
+
+//GLOBALS
+HWND gmWindow;
+HHOOK g_hook;
+HINSTANCE g_hThisDll;
+
+//DLL ENTRY
+int APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID IpReserved) {
+	if (dwReason == DLL_PROCESS_ATTACH) {
+		g_hThisDll = hInstance;
+		return DisableThreadLibraryCalls(g_hThisDll);
+	}
+
+	return TRUE;
+}
+
+//INTERNAL FUNCTIONS
+std::vector<std::wstring> ExtractPathList(HDROP hDrop){
+	std::vector<std::wstring> pathList;
+	UINT dropCount = DragQueryFile(hDrop, 0xFFFFFFFF, NULL, NULL);
+	
+	//std::cout << dropCount << " files dropped" << std::endl;
+
+	for (UINT i = 0; i < dropCount; i++) {
+		int bufferSize = DragQueryFile(hDrop, i, NULL, NULL) + 1;
+
+		WCHAR* path = new WCHAR[bufferSize];
+		DragQueryFile(hDrop, i, path, bufferSize); 
+
+		pathList.push_back(std::wstring(path));
+		delete [] path;
+	}
+	return pathList;
+}
+
+LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LPARAM lParam) {
+	PMSG pmsg = (PMSG)lParam;
+
+	if (0 > nCode || PM_NOREMOVE == wParam) {
+		return CallNextHookEx(g_hook, nCode, wParam, lParam);
+	}
+
+	if (pmsg->message == WM_DROPFILES) {
+		HDROP hDrop = (HDROP)pmsg->wParam;
+		
+		std::vector<std::wstring> pathList = ExtractPathList(hDrop);
+		int map = CreateDsMap(0);
+		UINT dropCount = pathList.size();
+		DsMapAddDouble(map, "dropCount", dropCount);
+		for (UINT i = 0; i < dropCount; i++) {
+			DsMapAddString(map,(char*)std::to_string(i).c_str(), (char*)utf8_encode(pathList[i]).c_str());
+		}
+		CreateAsynEventWithDSMap(map, EVENT_OTHER_SOCIAL);
+
+		DragFinish(hDrop);
+	}
+
+	return CallNextHookEx(g_hook, nCode, wParam, lParam);
+}
+
+//EXPORTED FUNCTIONS
+GMS_DLL double gms_filedrop_init() {
+	gmWindow = GetActiveWindow();
+	g_hook = SetWindowsHookEx(WH_GETMESSAGE, (HOOKPROC)HookCallback, g_hThisDll, 0);
+	DragAcceptFiles(gmWindow, true);
+	std::cout << "File drop initialized" << std::endl;
+	return 0;
+}
+
+GMS_DLL double gms_filedrop_free() {
+	UnhookWindowsHookEx(g_hook);
+	g_hook = NULL;
+	DragAcceptFiles(gmWindow, false);
+	std::cout << "File drop freed" << std::endl;
+	return 0;
+}
+
+/*
+GMS_DLL double gms_filedrop_error_check() {
+	ErrorCheck();
+	return 0;
+}
+
+GMS_DLL double gms_buffer_fiddle(char* buffer, double size) {
+	for (int i = 0; i < size; ++i) {
+		buffer[i] = size - i;
+	}
+
+	return 0;
+}
+*/
